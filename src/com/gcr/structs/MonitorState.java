297b35c9bb5396ec8e32d503cc1506bdd05d0296
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2013-12-20 23:49:43 +0530

    javadoc comments added to new enum

--- src/com/gcr/structs/MonitorState.java
+++ src/com/gcr/structs/MonitorState.java
@@ -1,5 +1,25 @@
 package com.gcr.structs;
 
+/**
+ * The Enum MonitorState is used by monitors to reflect their state.
+ */
 public enum MonitorState {
-	NEW, RUNNING, HELD, TERMINATED;
+
+	/**
+	 * The new state represents a monitor that has been instantiated but the
+	 * monitoring has not been started.
+	 */
+	NEW,
+	/** The running state means that the monitoring thread is running. */
+	RUNNING,
+	/**
+	 * The held state means that the monitoring thread has been halted
+	 * artificially.
+	 */
+	HELD,
+	/**
+	 * The terminated state means that the monitor has completed it monitoring
+	 * and the monitoring thread has stopped after completing its job.
+	 */
+	TERMINATED;
 }
