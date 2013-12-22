3823e8abb45114ee08a79b436972850c9677bc1e
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2013-12-22 15:30:35 +0530

    Lisence added to new file

--- src/com/gcr/structs/MonitorState.java
+++ src/com/gcr/structs/MonitorState.java
@@ -1,3 +1,18 @@
+/*This file is part of gcRadar.
+
+gcRadar is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by 
+the Free Software Foundation version 3 of the License.
+
+gcRadar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with gcRadar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.gcr.structs;
 
 /**
