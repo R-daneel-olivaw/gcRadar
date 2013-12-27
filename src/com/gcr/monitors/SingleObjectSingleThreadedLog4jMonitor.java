70a7f8297b28f2a5a18a12978233c02f348fc098
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2013-12-27 23:27:46 +0530

    Monitor thread yeild method added to SingleObjectSingleThreadedLog4jMonitor

--- src/com/gcr/monitors/SingleObjectSingleThreadedLog4jMonitor.java
+++ src/com/gcr/monitors/SingleObjectSingleThreadedLog4jMonitor.java
@@ -24,6 +24,7 @@ import org.apache.log4j.Logger;
 import com.gcr.monitors.modules.in.impl.InputModule;
 import com.gcr.monitors.modules.monitoring.impl.MonitoringModule;
 import com.gcr.monitors.modules.monitoring.structs.MonitorStateEnum;
+import com.gcr.monitors.modules.monitoring.structs.MonitorThreadYeildController;
 import com.gcr.monitors.modules.notification.NotificationModuleInterface;
 import com.gcr.structs.AbstractObjectRefrenceKey;
 
@@ -290,6 +291,11 @@ public class SingleObjectSingleThreadedLog4jMonitor<I> {
 	 */
 	private boolean isMonitorReady() {
 		return (state == MonitorStateEnum.RUNNING || state == MonitorStateEnum.NEW);
+	}
+	
+	public void setMonitorThreadYeildController(MonitorThreadYeildController yeildController)
+	{
+		monitoringMod.setMonitorThreadYeildController(yeildController);
 	}
 
 	// ****************** INNER-CLASSES ***********************
